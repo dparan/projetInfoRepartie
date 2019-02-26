813e48a05c125efa674be941b3b90116b0b070ef
Hrafven <hrafven@gmail.com>
2019-02-26 09:22:23 +0100

    ajout de la structure du projet

---
+++ src/main/java/org/ir/agents/aiguilleur/App.java
@@ -0,0 +1,13 @@
+package org.ir.agents.aiguilleur;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
