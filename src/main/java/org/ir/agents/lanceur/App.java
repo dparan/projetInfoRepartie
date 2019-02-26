ab3b4e14620acdddeab52fd69e131eee29d3b33b
Hrafven <hrafven@gmail.com>
2019-02-26 09:16:47 +0100

    ajout de la structure du projet

---
+++ src/main/java/org/ir/agents/lanceur/App.java
@@ -0,0 +1,13 @@
+package org.ir.agents.lanceur;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
