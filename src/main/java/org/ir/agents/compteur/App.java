5bc7efd660885476791c73acf685d649beb71351
Hrafven <hrafven@gmail.com>
2019-02-26 09:24:23 +0100

    ajout de la structure du projet

---
+++ src/main/java/org/ir/agents/compteur/App.java
@@ -0,0 +1,13 @@
+package org.ir.agents.compteur;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
