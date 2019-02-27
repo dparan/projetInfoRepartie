e6f7dc585ad48b6b9c2a4bd01e59e7e884b78cf0
Soufiane Hafiane <soufiane.hafiane@etudiant.univ-lr.fr>
2019-02-27 14:24:56 +0100

    ajout de la classe AgentMobile

---
+++ src/main/java/org/ir/agents/mobile/AgentMobile.java
@@ -0,0 +1,95 @@
+import java.io.*;
+import java.net.*;
+
+
+
+
+public class AgentMobile extends ObjectInputStream{
+
+
+
+	private ClassLoader remoteObjectClassLoader = null;
+    private final String id;
+    private String content;
+    private Integer direction = 0;
+    private static final int MIN = 0;
+    private static final int MAX = Integer.MAX_VALUE;
+
+    public AgentMobile(String id, String content, Integer direction,InputStream is, URL classpath) throws UnknownHostException {
+        if (id != null) {
+            this.id = id;
+        } else {
+        	
+            InetAddress inetAddress = InetAddress.getLocalHost();
+            int nombreAleatoire = MIN + (int) (Math.random() * ((MAX - MIN) + 1));
+            Date now = new Date();
+            long nowTimeStamp = now.toInstant().toEpochMilli();
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append(inetAddress.getHostName()).append(nombreAleatoire).append(nowTimeStamp);
+            this.id = stringBuilder.toString();
+        }
+        this.content = content;
+        if (direction != null) {
+            this.direction = direction;
+        }
+        super(is);
+			URL[] urls = {classpath};
+			remoteObjectClassLoader = new URLClassLoader(urls);
+    }
+    protected Class resolveClass(ObjectStreamClass desc)
+		throws IOException, ClassNotFoundException {
+		return remoteObjectClassLoader.loadClass(desc.getName());
+		}
+    public AgentMobile(String content) throws UnknownHostException {
+        this(null, content, null);
+    }
+
+    public AgentMobile(String content, int direction) throws UnknownHostException {
+        this(null, content, direction);
+    }
+
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @return the content
+     */
+    public String getContent() {
+        return content;
+    }
+
+    /**
+     * @return the direction
+     */
+    public int getDirection() {
+        return direction;
+    }
+
+    /**
+     * @param content the content to set
+     */
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    /**
+     * @param direction the direction to set
+     */
+    public void setDirection(int direction) {
+        this.direction = direction;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(direction).append("-").append(id).append("-").append(content);
+        return stringBuilder.toString();
+    }
+
+}
+
+}
