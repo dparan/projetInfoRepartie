eec1ecc6130a3bc819395c19501701bd98ab9ad4
Soufiane Hafiane <soufiane.hafiane@etudiant.univ-lr.fr>
2019-02-27 11:58:39 +0100

    Ajout de commentaires et mise à jour du gitignore pour macOS

--- src/main/java/org/ir/agents/lanceur/App.java
+++ src/main/java/org/ir/agents/lanceur/App.java
@@ -19,19 +19,62 @@ import java.util.Scanner;
  *
  */
 public class App {
+    /**
+    *
+    *       <p> Variable statique présente une instance de la classe ReqRunnable </p>
+    *         @see  ReqRunnable
+    *
+    */
     private static ReqRunnable imageRunnable = new ReqRunnable();
+     /**
+    *
+    *       <p> Variable statique présente une instance de la classe ReqRunnable </p>
+    *       @see ReqRunnable
+    *
+    */
     private static ReqRunnable texteRunnable = new ReqRunnable();
+     /**
+    *
+    *       <p> variable statique présente une instance de l'interface Runnable. </p>
+    *         <p> implémentée par toute classe dont les instances sont destinées à être exécutées par un thread. </p>
+    *
+    */
     private static Runnable serveur = new Runnable() {
 
         @Override
+        /**
+            * <lu>Methode lancée par un thread </lu>
+            *  <li>attend que les demandes arrivent sur le réseau. </li>
+            * 
+            *
+        */
         public void run() {
             try {
                 ServerSocket socket = new ServerSocket(1102);
+                /**
+                *   <p> Boucle while pour la reception des mesages saisi par l'utilisateur </p>
+                *
+                */
                 while (true) {
+                    /**
+                    *
+                    *  @see ReqRunnable#socket
+                    */
                     Socket socketClient = socket.accept();
+                    /**
+                    *
+                    *  @see ReqRunnable#PrintWriter
+                    */
                     PrintWriter out = new PrintWriter(socketClient.getOutputStream(), true);
+                    /**
+                    *
+                    *   @see ReqRunnable#BufferedReader
+                    */
                     BufferedReader in = new BufferedReader(new InputStreamReader(socketClient.getInputStream()));
-
+                    /**
+                    *  <p> Traitement de la saisie </p>
+                    *
+                    */
                     switch (in.readLine()) {
                     case "texte":
                         out.println("Du Texte");
@@ -52,25 +95,62 @@ public class App {
             }
         }
     };
-
+     /**
+    *
+    *    <p> Methode parmet l'affichage </P> 
+    *
+    *
+    */
     private static void display() {
         System.out.println("1-\tImage\n2-\tTexte\n3-\tQuitter\n");
     }
 
     public static void main(String[] args) {
 
+         /**
+    *
+    *       <p> Instance d'un thread </p>
+    *       @param serveur 
+    *       @see App#serveur 
+    *
+    */
         Thread serveurThread = new Thread(serveur);
+         /**
+    *
+    *       <p> Lancement du thread </p>
+    *
+    *
+    */
         serveurThread.start();
+         /**
+    *
+    *       @see ReqRunnable#path
+    *       @see ReqRunnable#configure
+    *
+    */
 
         imageRunnable.configure("image");
+         /**
+    *
+    *       @see ReqRunnable#path
+    *       @see ReqRunnable#configure
+    *
+    */
         texteRunnable.configure("texte");
+         /**
+    *
+    *       <p> Traitement de la saisie </p>
+    *
+    *
+    */
 
         Scanner sc = new Scanner(System.in);
         while (true) {
             display();
             while (!sc.hasNextInt()) {
-                // prise en charge des erreurs de saisie
-                // l'entrée n'est pas un entier
+                /** <p>Prise en charge des erreurs de saisie. </p>
+                * <p> l'entrée n'est pas un entier. </p>
+                */
                 System.out.println("Veuillez saisir 1, 2 ou 3");
                 display();
                 sc.nextLine();
