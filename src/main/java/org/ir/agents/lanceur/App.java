1a1b5412b93d38e35557f0acabcaadd2f61f0e8b
Bidois Hugo <hbidois@frodon05.univ-lr.fr>
2019-02-27 12:10:46 +0100

    Corrections orthographe et formatage du code

--- src/main/java/org/ir/agents/lanceur/App.java
+++ src/main/java/org/ir/agents/lanceur/App.java
@@ -7,92 +7,104 @@ import java.util.Scanner;
  * 
  * @version 1.0
  *
- * Permet de choisir le type de requête à propager Les choix sont : 
- *          1 - Images 
- *          2 - Texte 
- * Les réponses attendues sont : 
- *          1 - Une Image
- *          2 - Du Texte
+ *          Permet de choisir le type de requête à propager Les choix sont : 1 -
+ *          Images 2 - Texte Les réponses attendues sont : 1 - Une Image 2 - Du
+ *          Texte
  *
  */
 public class App {
     /**
-    *
-    *       <p> Variable statique présente une instance de la classe ReqRunnable </p>
-    *         @see  ReqRunnable
-    *
-    */
+     *
+     * <p>
+     * Variable statique représentant une instance de la classe ReqRunnable pour les images
+     * </p>
+     * 
+     * @see ReqRunnable
+     *
+     */
     private static ReqRunnable imageRunnable = new ReqRunnable();
-     /**
-    *
-    *       <p> Variable statique présente une instance de la classe ReqRunnable </p>
-    *       @see ReqRunnable
-    *
-    */
+    /**
+     *
+     * <p>
+     * Variable statique représentant une instance de la classe ReqRunnable pour le texte
+     * </p>
+     * 
+     * @see ReqRunnable
+     *
+     */
     private static ReqRunnable texteRunnable = new ReqRunnable();
 
-     /**
-    *
-    *    <p> Methode parmet l'affichage </P> 
-    *
-    *
-    */
+    /**
+     *
+     * <p>
+     * Methode permetant l'affichage
+     * </P>
+     *
+     *
+     */
     private static void display() {
         System.out.println("1-\tImage\n2-\tTexte\n3-\tQuitter\n");
     }
 
     public static void main(String[] args) {
-    /**
-    *
-    *       @see ReqRunnable#path
-    *       @see ReqRunnable#configure
-    *
-    */
+        /**
+         *
+         * @see ReqRunnable#path
+         * @see ReqRunnable#configure
+         *
+         */
 
         imageRunnable.configure("0-0-image");
-    /**
-    *
-    *       @see ReqRunnable#path
-    *       @see ReqRunnable#configure
-    *
-    */
+        /**
+         *
+         * @see ReqRunnable#path
+         * @see ReqRunnable#configure
+         *
+         */
         texteRunnable.configure("0-0-text");
         /**
-        *
-        *       <p> Traitement de la saisie </p>
-        *
-        *
-        */
+         *
+         * <p>
+         * Traitement de la saisie
+         * </p>
+         *
+         *
+         */
         Scanner sc = new Scanner(System.in);
         while (true) {
             // affichage du menu et saisie de la valeur
             display();
             while (!sc.hasNextInt()) {
-                /** <p>Prise en charge des erreurs de saisie. </p>
-                * <p> l'entrée n'est pas un entier. </p>
-                */
+                /**
+                 * <p>
+                 * Prise en charge des erreurs de saisie.
+                 * </p>
+                 * <p>
+                 * l'entrée n'est pas un entier.
+                 * </p>
+                 */
                 System.out.println("Veuillez saisir 1, 2 ou 3");
                 display();
                 sc.nextLine();
             }
             int num = sc.nextInt();
             Thread reqThread = null;
-            
+
             switch (num) {
             case 1:
-                // On souhaite avoir une image 
+                // On souhaite avoir une image
                 reqThread = new Thread(imageRunnable);
                 break;
             case 2:
-                // On souhaite avoir une image
+                // On souhaite avoir du texte
                 reqThread = new Thread(texteRunnable);
                 break;
             case 3:
-                // fin du programme 
+                // fin du programme
                 System.exit(0);
                 break;
             default:
-                //  Erreur de saisie
+                // Erreur de saisie
                 System.out.println("Veuillez saisir 1, 2 ou 3");
                 sc.nextLine();
                 break;
--- src/main/java/org/ir/agents/lanceur/ReqRunnable.java
+++ src/main/java/org/ir/agents/lanceur/ReqRunnable.java
@@ -7,51 +7,69 @@ import java.io.PrintWriter;
 import java.net.Socket;
 
 /**
- * <p> ReqRunnable implemente  Runnable.</p>
- *  <p> elle gere la communication avec le serveur.</p>
+ * <p>
+ * ReqRunnable implemente Runnable.
+ * </p>
+ * <p>
+ * elle gere la communication avec le serveur.
+ * </p>
  */
 public class ReqRunnable implements Runnable {
     /**
-    *
-    * <p> Permet a se connecter à une machine distante afin de communiquer </p>
-    *
-    *
-    */
+     *
+     * <p>
+     * Permet a se connecter à une machine distante afin de communiquer
+     * </p>
+     *
+     *
+     */
 
     private Socket socket;
     /**
-    *      <p> Chaine de caractères sous forme d'un path. Ce path est modifiable </p>
-    *
-    *       @see ReqRunnable#configure
-    *
-    */
+     * <p>
+     * Chaine de caractères sous la forme <direction>-<id>-<message>, ex: 0-1-text. Ce path est modifiable
+     * </p>
+     *
+     * @see ReqRunnable#configure
+     *
+     */
     private String path;
     /**
-    *
-    *   <p> Impression des représentations formatées d'objets dans un flux de sortie de texte. </p>
-    *
-    *
-    *
-    */
+     *
+     * <p>
+     * Impression des représentations formatées d'objets dans un flux de sortie de
+     * texte.
+     * </p>
+     *
+     *
+     *
+     */
     private PrintWriter out;
     /**
-    *
-    *  <p> Lecture du texte d'un flux de saisie de caractères </p>
-    *
-    */
+     *
+     * <p>
+     * Lecture du texte d'un flux de saisie de caractères
+     * </p>
+     *
+     */
     private BufferedReader in;
+
     /**
-    *       Met à jour le path envoyé
-    *@param path
-    *            Le nouveau path à envoyer
-    **/
+     * Met à jour le path envoyé
+     * 
+     * @param path Le nouveau path à envoyer
+     **/
 
     public void configure(String path) {
         // valeur qui sera envoyé aux aiguilleurs
-        // "text" ou "image"
         this.path = path;
     }
 
+    /**
+     * Méthode permettant d'afficher un message reçu des aiguilleurs
+     * 
+     * @param message reçu depuis un socket d'aiguilleur
+     */
     public void parseAndDisplayMessage(String message) {
         String data[] = message.split("-");
         int direction = Integer.parseInt(data[0]);
@@ -62,37 +80,43 @@ public class ReqRunnable implements Runnable {
 
     @Override
     /**
-    * Metthode lancée par un thread 
-    * 
-    *
-    */
+     * Methode lancée par un thread
+     * 
+     *
+     */
     public void run() {
         // Prise en charge de la demande d'image
         try {
             /**
-            *
-            * <p>Création d'un socket de flux sur lecalhost et le connecte au numéro de port 1102.</p>
-            * @see ReqRunnable#socket 
-            */
+             *
+             * <p>
+             * Création d'un socket de flux sur localhost et le connecte au numéro de port
+             * 1102.
+             * </p>
+             * 
+             * @see ReqRunnable#socket
+             */
             socket = new Socket("localhost", 1102);
             /**
-            *   <p> un nouveau PrintWriter  d’un OutputStream existant. </p> 
-            * 
-            *   @param getOutputStream
-            *                          Retourne un flux de sortie pour ce socket.
-            *   @see ReqRunnable#PrintWriter
-            *   @see ReqRunnable#socket
-            */
+             * <p>
+             * un nouveau PrintWriter d’un OutputStream existant.
+             * </p>
+             * 
+             * @param getOutputStream Retourne un flux de sortie pour ce socket.
+             * @see ReqRunnable#PrintWriter
+             * @see ReqRunnable#socket
+             */
             out = new PrintWriter(socket.getOutputStream(), true);
-             /**
-            *    <p> Création d'un flux d'entrée de caractère en mémoire. </p>
-            *    @parm InputStreamReader
-            *                    Permettre la conversion d'octes en caractères.
-            *    @parm getInputStream 
-            *                       Retourne un flux d'entrée pour cette socket.
-            *   @see ReqRunnable#BufferdReader
-            *   @see ReqRunnable#socket
-            */
+            /**
+             * <p>
+             * Création d'un flux d'entrée de caractère en mémoire.
+             * </p>
+             * 
+             * @parm InputStreamReader Permettre la conversion d'octes en caractères.
+             * @parm getInputStream Retourne un flux d'entrée pour cette socket.
+             * @see ReqRunnable#BufferdReader
+             * @see ReqRunnable#socket
+             */
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             // Ecriture du message dans le flux de sortie
             out.println(path);
