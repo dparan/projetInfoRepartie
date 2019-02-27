accaab6cedb2e38c127b8f604a06001378ad1768
Bidois Hugo <hbidois@frodon05.univ-lr.fr>
2019-02-27 08:58:32 +0100

    Ajout de commentaires et refactorisation

--- src/main/java/org/ir/agents/compteur/App.java
+++ src/main/java/org/ir/agents/compteur/App.java
@@ -7,64 +7,92 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+/**
+ * 
+ * <p>Un agent compteur. Il contient les valeurs du compteur de texte et d'image.</p>
+ * <p>On utilise deux compteurs pour éviter d'impacter le poid d'un chemin en cas d'erreur.</p>
+ * <p>Par exemple si une requête pour le texte atteint le serveur d'image, on ne veut pas augmenter le compteur pour le texte.
+ * Cependant on ne veut pas impacter les pheromones des images.
+ * Par conséquence le chemin menant au mauvais serveur pour le texte perdera de son poid petit à petit sans le réduire pour les images.</p>
+ * 
+ * @version 1.0
+ */
 public class App {
 
-    final static Compteur compteurText = new Compteur();
-    final static Compteur compteurImage = new Compteur();
+    /**
+     * Un compteur pour les pheromones du texte.
+     */
+    private static Compteur compteurText = new Compteur();
+    /**
+     * Un compteur pour les pheromones des images.
+     */
+    private static Compteur compteurImage = new Compteur();
+    /**
+     * Runnable exécutant la logique du serveur.
+     */
     private static Runnable serveur = new Runnable() {
-//
+
+        private void handling(ServerSocket socket) throws IOException {
+            while (true) {
+                Socket socketClient = socket.accept();
+                PrintWriter out = new PrintWriter(socketClient.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(socketClient.getInputStream()));
+                String str = in.readLine();
+                switch (str) {
+                case "T+":
+                    compteurText.incrementer();
+                    System.out.println("Inc Text ok...");
+                    break;
+                case "T-":
+                    compteurText.decrementation();
+                    System.out.println("Dec Text ok...");
+                    break;
+                case "I+":
+                    compteurImage.incrementer();
+                    System.out.println("Inc Image ok...");
+                    break;
+                case "I-":
+                    compteurImage.decrementation();
+                    System.out.println("Dec Image ok...");
+                    break;
+                case "T":
+                    out.println(compteurText.getPH());
+                    System.out.println("print Text " + compteurText.getPH());
+                    break;
+                case "I":
+                    out.println(compteurImage.getPH());
+                    System.out.println("print Image " + compteurImage.getPH());
+                    break;
+                default:
+                    System.err.println("Erreur entrée invalide");
+                    out.println(-1);
+                    break;
+                }
+            }
+        }
+
         @Override
         public void run() {
+            ServerSocket socket = null;
             try {
-                ServerSocket socket = new ServerSocket(1102);
-                while (true) {
-                    Socket socketClient = socket.accept();
-                    PrintWriter out = new PrintWriter(socketClient.getOutputStream(), true);
-                    BufferedReader in = new BufferedReader(new InputStreamReader(socketClient.getInputStream()));
-                    String str = in.readLine();
-                    switch (str) {
-                        case "T+":
-                            compteurText.incrementer();
-                            System.out.println("Inc Text ok...");
-                            break;
-                        case "T-":
-                            compteurText.decrementation();
-                            System.out.println("Dec Text ok...");
-                            break;
-                        case "I+":
-                            compteurImage.incrementer();
-                            System.out.println("Inc Image ok...");
-                            break;
-                        case "I-":
-                            compteurImage.decrementation();
-                            System.out.println("Dec Image ok...");
-                            break;
-                        case "T":
-                            out.println(compteurText.getPH());  
-                            break;
-                        case "I":
-                            out.println(compteurImage.getPH());
-                            break;
-                        default:
-                            System.out.println("Erreur");
-                            break;
+                socket = new ServerSocket(1102);
+                handling(socket);
+            } catch (IOException e) {
+                System.err.println("Fermeture du socket sur erreur");
+                if (socket != null) {
+                    try {
+                        socket.close();
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
                     }
                 }
-
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
             }
         }
     };
 
-    ;
     public static void main(String[] args) throws InterruptedException {
-
         Thread serveurThread = new Thread(serveur);
         serveurThread.start();
-//        double ph = 0.5;
         serveurThread.join();
     }
 }
-
--- src/main/java/org/ir/agents/compteur/Compteur.java
+++ src/main/java/org/ir/agents/compteur/Compteur.java
@@ -1,14 +1,35 @@
 package org.ir.agents.compteur;
 
+/**
+ * Modèle d'un compteur.
+ * 
+ * @version 1.0
+ */
 public class Compteur {
+    /**
+     * La valeur du compteur (pheromone)
+     */
     private double ph = 15;
-    final static double ALPHA = 0.1;
-    final static double BETA = 0.2;
+    /**
+     * La valeur α utilisée pour le calcul de la décrémentation
+     */
+    public static final double ALPHA = 0.1;
+    /**
+     * La valeur β utilisée pour le calcul de l'incrémentation
+     */
+    public static final double BETA = 0.2;
 
+    /**
+     * Méthode d'incrémentation du compteur ph. Ph(t + 1) = Ph(t) + β
+     * 
+     */
     public synchronized void incrementer() {
         ph += BETA;
     }
 
+    /**
+     * Méthode de décrémentation du compteur ph. Ph(t + 1) = (1 - α) * Ph(t)
+     */
     public synchronized void decrementation() {
         ph = (1 - ALPHA) * ph;
     }
