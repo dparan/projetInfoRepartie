9f4e99b856e62ad7d0983f2df90da2adc78fbeec
Derouet Jonathan <jderou01@gollum07.univ-lr.fr>
2019-02-28 14:22:16 +0100

    fix warnings

--- src/main/java/org/ir/agents/lanceur/ReqRunnable.java
+++ src/main/java/org/ir/agents/lanceur/ReqRunnable.java
@@ -1,12 +1,8 @@
 package org.ir.agents.lanceur;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
 /**
  * <p>
